351dd4b69b40ef9a3b9110520bf35c78fb683b6b
Vincent Bonmalais <vb.kouno@gmail.com>
2013-08-20 10:48:25 +0400

    Add aliases for CLI tool.

--- lib/monitrb/cli.rb
+++ lib/monitrb/cli.rb
@@ -2,9 +2,9 @@ require 'thor'
 
 module MonitRB
   class CLI < ::Thor
-    desc "build", "generate monit files"
-    method_option :path,   type: :string, default: '', required: true
-    method_option :output, type: :string, default: '', required: true
+    desc "build", "Generate monit files"
+    method_option :path,   type: :string, default: '', required: true, aliases: '-p',
+    method_option :output, type: :string, default: '', required: true, aliases: '-o'
     def build
       Dir.glob(options.path).each do |path|
         load path
