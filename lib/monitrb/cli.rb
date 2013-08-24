b4df5d6862dc28e6dfa1797762bc0988e20efddf
Vincent Bonmalais <vb.kouno@gmail.com>
2013-08-24 13:11:59 +0400

    Woops, syntax error.

--- lib/monitrb/cli.rb
+++ lib/monitrb/cli.rb
@@ -3,7 +3,7 @@ require 'thor'
 module MonitRB
   class CLI < ::Thor
     desc "build", "Generate monit files"
-    method_option :path,   type: :string, default: '', required: true, aliases: '-p',
+    method_option :path,   type: :string, default: '', required: true, aliases: '-p'
     method_option :output, type: :string, default: '', required: true, aliases: '-o'
     def build
       Dir.glob(options.path).each do |path|
