1a9ef69290c6bb6861baa85183f8f424bab24572
Vincent Bonmalais <vb.kouno@gmail.com>
2013-08-25 10:31:37 +0400

    Remove useless variables.
    This should have been removed when I refactored the code to add
    fixtures.

--- spec/lib/generator_spec.rb
+++ spec/lib/generator_spec.rb
@@ -3,20 +3,6 @@ require 'spec_helper'
 describe MonitRB::Generator do
   subject { MonitRB::Generator }
 
-  let(:name)          { 'resque' }
-  let(:type)          { :process }
-  let(:pid_file)      { '/path/to/pidfile' }
-  let(:shell_command) { '/bin/sh -l -c' }
-  let(:pwd)           { '/srv/APP_NAME/current' }
-  let(:start)         { "nohup bundle exec rake environment resque:work >> log/resque_worker_QUEUE.log 2>&1" }
-  let(:stop)          { "kill -9 $(cat /path/to/pidfile) && rm -f /path/to/pidfile; exit 0;" }
-  let(:conditions)    { [ "if totalmem > 100.0 MB for 5 cycles then restart " ] }
-  let(:env)           { { home: '/home/user',
-                          rails_env: 'production',
-                          queue: 'queue_name',
-                          verbose: '1',
-                          pidfile: '/path/to/pidfile' } }
-
   let(:expected_conf) { File.read(fixtures_path('monit', 'resque.monitrc')) }
 
   before(:each) do
