a77a218f83da7a30d1999a722b1fc6c5dcc3b05c
Vincent Bonmalais <vb.kouno@gmail.com>
2013-08-27 23:49:08 +0400

    Verify there is support for multi-parameters with paths.

--- spec/lib/cli_spec.rb
+++ spec/lib/cli_spec.rb
@@ -2,26 +2,29 @@ require 'spec_helper'
 
 describe MonitRB::CLI do
   before(:each) do
-    File.open(input_file, 'w') { |f| f.write(config) }
+    FileUtils.cp([fixtures_path(%w{ monitrb ssh.rb }), fixtures_path(%w{ monitrb resque.rb })], tmp_path)
   end
 
   after(:each) do
-    File.delete(input_file)  if File.exists?(input_file)
     File.delete(output_file) if File.exists?(output_file)
   end
 
-  let(:input_file)  { tmp_path('input.rb') }
   let(:output_file) { tmp_path('output.monitrc') }
   let(:config)      { File.read(fixtures_path('monitrb', 'resque.rb')) }
 
   describe "::build" do
     it "creates a monit configuration file" do
-      MonitRB::CLI.start %w{ build --path=tmp/input.rb --output=tmp/output.monitrc }
+      MonitRB::CLI.start %w{ build --path=tmp/resque.rb --output=tmp/output.monitrc }
       expect(File.exists?(output_file)).to be_true
     end
 
     it "can use wildcards to use multiple files" do
       MonitRB::CLI.start %w{ build --path=tmp/*.rb --output=tmp/output.monitrc }
+      expect(File.exists?(output_file)).to be_true
+    end
+
+    it "can use multiple arguments for paths" do
+      MonitRB::CLI.start %w{ build -p tmp/ssh.rb,tmp/*.rb -o tmp/output.monitrc }
       expect(File.exists?(output_file)).to be_true
     end
   end
