ad20e2fb9baf63b62510130507629d9f6c353e13
Vincent Bonmalais <vb.kouno@gmail.com>
2013-09-02 22:29:44 +0400

    Add directory checks.

--- spec/lib/type_spec.rb
+++ spec/lib/type_spec.rb
@@ -1,9 +1,15 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe MonitRB::Config, "Supported checks" do
-  it "supports file type" do
+  it "supports file checks" do
     load fixtures_path(%w{ monitrb file.rb })
     expected_conf = File.read(fixtures_path(%w{ monit file.monitrc }))
+    expect(MonitRB::Config.stack.first.to_s).to eq(expected_conf)
+  end
+
+  it "supports directory check" do
+    load fixtures_path(%w{ monitrb directory.rb })
+    expected_conf = File.read(fixtures_path(%w{ monit directory.monitrc }))
     expect(MonitRB::Config.stack.first.to_s).to eq(expected_conf)
   end
 end
